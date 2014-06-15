a099e6397dfa042b3345cf38287201bee570bb84
Tomasz <mode.off@gmail.com>
2014-06-15 11:34:37 +0200

    little error

--- plugins/slap.py
+++ plugins/slap.py
@@ -2,8 +2,8 @@ import mfunctions, mconfig, random
 def receive(line, bot_socket):
   text = mfunctions.user_text(line)
   if(text):
-    if(text[1][:5] == "!slap" and len(text[1]) > 6 and text[0] != mconfig.NICK):
-      mfunctions.say("\x01ACTION "+slap(text[0])+"\x01", bot_socket)
+    if(text[1][:5] == "!slap" and len(text[1]) > 6 and text[1][6:] != mconfig.NICK):
+      mfunctions.say("\x01ACTION "+slap(text[1][6:])+"\x01", bot_socket)
 
 def slap(nick):
     plik = open("plugins/slap","r")
@@ -12,3 +12,4 @@ def slap(nick):
     tbl = tbl[0:len(tbl)-1]
     ran = random.randint(0,len(tbl)-1)
     return tbl[ran].replace('nick',nick)
+
