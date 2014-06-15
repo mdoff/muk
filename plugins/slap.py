257b33ecba56e619e9b39365cd7c71ec3ff9b38c
Tomasz <mode.off@gmail.com>
2014-06-15 11:29:44 +0200

    added slap  plugin

---
+++ plugins/slap.py
@@ -0,0 +1,14 @@
+import mfunctions, mconfig, random
+def receive(line, bot_socket):
+  text = mfunctions.user_text(line)
+  if(text):
+    if(text[1][:5] == "!slap" and len(text[1]) > 6 and text[0] != mconfig.NICK):
+      mfunctions.say("\x01ACTION "+slap(text[0])+"\x01", bot_socket)
+
+def slap(nick):
+    plik = open("plugins/slap","r")
+    tbl = plik.read()
+    tbl = tbl.split('\n')
+    tbl = tbl[0:len(tbl)-1]
+    ran = random.randint(0,len(tbl)-1)
+    return tbl[ran].replace('nick',nick)
