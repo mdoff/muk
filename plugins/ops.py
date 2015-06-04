1aa96324c6e1358566ef348b43b64777f66a66a4
mdoff <mdoff@mdoff.net>
2015-06-04 22:09:29 +0200

    Now muk can set topic

--- plugins/ops.py
+++ plugins/ops.py
@@ -1,6 +1,19 @@
 import mfunctions, mconfig, random
+
+"""
+  To perform action as OP send muk a private message in format:
+  <action> <password> (<optional>)
+  eg: !op qwerty
+      !topic qwerty New channel topic
+  actions:
+    !op   - gives user OP on channel
+    !topic  - set channel topic
+"""
 def receive(line, bot_socket):
   text = mfunctions.user_priv(line)
   if(text):
     if(text[1][:3] == "!op" and len(text[1]) > 4 and text[1][4:] == mconfig.OP_PASSWD):
       mfunctions.send("MODE "+ mconfig.CHANNEL+' +o ' +text[0]+"\r\n", bot_socket)
+    if( text[1][:6] == "!topic" and len(text[1]) > 4 and text[1][7:len(mconfig.OP_PASSWD)+7] == mconfig.OP_PASSWD ):
+      mline = "TOPIC "+ mconfig.CHANNEL+' :' +text[1][8+len(mconfig.OP_PASSWD):]+"\r\n"
+      mfunctions.send(mline, bot_socket)
